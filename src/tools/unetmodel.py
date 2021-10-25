ff098476e85a114f83a70bdc76556f9a69bd005e
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2021-10-25 10:23:24 +0000

    Made model construction less naive, slightly increase capacity

--- src/tools/unetmodel.py
+++ src/tools/unetmodel.py
@@ -6,166 +6,144 @@ from keras.layers.convolutional import Conv2D, Conv2DTranspose
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.merge import concatenate
 from keras import backend as K
+import tensorflow as tf
 
+def unet(channels, dropouts):
+    
+    def f(x):
+        # downwards
+        downwards = []
+        for dropout, channel in zip(dropouts, channels):
+            x = Conv2D(channel, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(x)
+            x = Dropout(dropout)(x)
+            x = Conv2D(channel, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(x)
+            downwards.append(x)
+            x = MaxPooling2D((2,2))(x)
+
+        # We drop the last max pooling
+        x = downwards[-1]
+        downward_signals = downwards[:-1]
+    
+        # Iterate upwards
+        for downward_signal, dropout, channel in list(zip(downward_signals, dropouts, channels))[::-1]:
+            x = Conv2DTranspose(channel, (2, 2), strides=(2, 2), padding='same') (x)
+            x = concatenate([x, downward_signal])
+            x = Conv2D(channel, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(x)
+            x = Dropout(dropout)(x)
+            x = Conv2D(channel, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(x)
+
+        return x
+    
+    return f
 
 def u_net_model(width, height, channels):
     inputs = Input((height, width, channels))
     s = Lambda(lambda x: x / 255) (inputs)
-
-    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
-    c1 = Dropout(0.1) (c1)
-    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
-    p1 = MaxPooling2D((2, 2)) (c1)
-
-    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
-    c2 = Dropout(0.1) (c2)
-    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
-    p2 = MaxPooling2D((2, 2)) (c2)
-
-    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
-    c3 = Dropout(0.2) (c3)
-    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
-    p3 = MaxPooling2D((2, 2)) (c3)
-
-    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
-    c4 = Dropout(0.2) (c4)
-    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
-    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
-
-    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
-    c5 = Dropout(0.3) (c5)
-    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
-
-    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
-    u6 = concatenate([u6, c4])
-    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
-    c6 = Dropout(0.2) (c6)
-    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
-
-    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
-    u7 = concatenate([u7, c3])
-    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
-    c7 = Dropout(0.2) (c7)
-    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
-
-    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
-    u8 = concatenate([u8, c2])
-    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
-    c8 = Dropout(0.1) (c8)
-    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
-
-    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
-    u9 = concatenate([u9, c1], axis=3)
-    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-    c9 = Dropout(0.1) (c9)
-    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
-
-    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+    
+    dropouts = [0.1, 0.1, 0.2, 0.2, 0.3]
+    channels = [16, 32, 64, 128, 256]
+    
+    x = unet(channels, dropouts)(s)
+    
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (x)
 
     model = Model(inputs=[inputs], outputs=[outputs])
     model.compile(optimizer='adam', loss='binary_crossentropy')
 
     return model
 
+
 def mean_squared_error_masked(y_true, y_pred):
     mask = K.sign(K.abs(y_true))
     return K.sum(K.square(y_pred - y_true) * mask, axis=-1) / (K.sum(mask, axis=-1) + 0.1)
 
-def u_net_model_ext(width, height, channels):
+def u_net_model_ext(width, height, channels, centers_weight=None):
     inputs = Input((height, width, channels))
     s = Lambda(lambda x: x / 255) (inputs)
+    
+    dropouts = [0.1, 0.1, 0.2, 0.2, 0.3]
+    #channels = [16, 32, 64, 128, 256]
+    channels = [32, 64, 128, 256, 512]
 
-    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
-    c1 = Dropout(0.1) (c1)
-    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
-    p1 = MaxPooling2D((2, 2)) (c1)
+    
+    x = unet(channels[:2], dropouts[:2])(s)
+    
 
-    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
-    c2 = Dropout(0.1) (c2)
-    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
-    p2 = MaxPooling2D((2, 2)) (c2)
-
-    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
-    c3 = Dropout(0.2) (c3)
-    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
-    p3 = MaxPooling2D((2, 2)) (c3)
-
-    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
-    c4 = Dropout(0.2) (c4)
-    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
-    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
-
-    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
-    c5 = Dropout(0.3) (c5)
-    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
-
-    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
-    u6 = concatenate([u6, c4])
-    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
-    c6 = Dropout(0.2) (c6)
-    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
-
-    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
-    u7 = concatenate([u7, c3])
-    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
-    c7 = Dropout(0.2) (c7)
-    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
-
-    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
-    u8 = concatenate([u8, c2])
-    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
-    c8 = Dropout(0.1) (c8)
-    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
-
-    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
-    u9 = concatenate([u9, c1], axis=3)
-
-    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-    c9 = Dropout(0.1) (c9)
-    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
-    outputs = Conv2D(1, (1, 1), activation='sigmoid', name='mask') (c9)
+    outputs = Conv2D(1, (1, 1), activation='sigmoid', name='mask') (x)
 
     # Predicting centers
-    ce9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-    ce9 = Dropout(0.1) (ce9)
-    ce9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (ce9)
-    centers = Conv2D(1, (1, 1), activation='sigmoid', name='centers') (ce9)
+    centers = Conv2D(1, (1, 1), activation='sigmoid', name='centers') (x)
+    overlaps = Conv2D(1, (1, 1), activation='sigmoid', name='overlaps') (x)
+    widths = Conv2D(1, (1, 1), activation='linear', name='widths') (x)
+    heights = Conv2D(1, (1, 1), activation='linear', name='heights') (x)
+    diffx = Conv2D(1, (1, 1), activation='linear', name='diffx') (x)
+    diffy = Conv2D(1, (1, 1), activation='linear', name='diffy') (x)
+    
+    if centers_weight:
+#        centers_loss = weighted_categorical_crossentropy([1.0, centers_weight])
+        centers_loss = weighted_categorical_crossentropy([centers_weight, centers_weight])
 
-    # Predicting widths
-    w9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-    w9 = Dropout(0.1) (w9)
-    w9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (w9)
-    widths = Conv2D(1, (1, 1), activation='linear', name='widths') (ce9)
+    else:
+        centers_loss = 'binary_crossentropy'
+    centers_loss = weighted_categorical_crossentropy([1.0, 1.0])
 
-    # Predicting heights
-    h9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-    h9 = Dropout(0.1) (h9)
-    h9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (h9)
-    heights = Conv2D(1, (1, 1), activation='linear', name='heights') (h9)
 
-    # Predicting diffx
-    dx9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-    dx9 = Dropout(0.1) (dx9)
-    dx9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (dx9)
-    diffx = Conv2D(1, (1, 1), activation='linear', name='diffx') (dx9)
-
-    # Predicting heights
-    dy9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-    dy9 = Dropout(0.1) (dy9)
-    dy9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (dy9)
-    diffy = Conv2D(1, (1, 1), activation='linear', name='diffy') (dy9)
-
-    model = Model(inputs=[inputs], outputs=[outputs, centers, widths, heights, diffx, diffy])
+    model = Model(inputs=[inputs], outputs=[outputs, centers, overlaps, widths, heights, diffx, diffy])
     model.compile(optimizer='adam',
                   loss={
                       'mask': 'binary_crossentropy',
                       'centers': 'binary_crossentropy',
+                      'overlaps': 'binary_crossentropy',
+#                      'centers': centers_loss,
+#                      'overlaps': centers_loss,
                       'widths': mean_squared_error_masked,
                       'heights': mean_squared_error_masked,
                       'diffx': mean_squared_error_masked,
                       'diffy': mean_squared_error_masked
+                  },
+                  loss_weights={
+                      'mask': 50,
+                      'centers': 10000,
+                      'overlaps': 50,
+                      'widths': 150,
+                      'heights': 150,
+                      'diffx': 150,
+                      'diffy': 150
                   })
+
 
     return model
 
+
+#
+# Reference: https://gist.github.com/wassname/ce364fddfc8a025bfab4348cf5de852d
+#
+def weighted_categorical_crossentropy(weights):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+    
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+    
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+    
+    weights = K.variable(weights)
+        
+    def parametrized_weighted_loss(y_true, y_pred):
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # done to make the target label type compatible
+        y_true = tf.cast(y_true, y_pred.dtype)
+        # calc
+        loss = y_true * K.log(y_pred) * weights
+        loss = -K.sum(loss, -1)
+        return loss
+    
+    return parametrized_weighted_loss
 
