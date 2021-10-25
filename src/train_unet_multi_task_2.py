d7fd2905ef0448bb012495dd9c93c081e52f0a62
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2021-10-25 10:26:33 +0000

    Training script for new extended model

---
+++ src/train_unet_multi_task_2.py
@@ -0,0 +1,31 @@
+import argparse
+
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+from tools import sources
+from tools import unetmodel
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--output", type=str, required=True)
+parser.add_argument("--samples", type=int)
+parser.add_argument("--epochs", type=int, default=2)
+args = parser.parse_args()
+
+
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+
+tx, ty, tc, to, tdx1, tdy1, tdx2, tdy2, _, _ = sources.flattened_trainset_ex_2(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS, args.samples)
+model = unetmodel.u_net_model_ext(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS, centers_weight=10.0)
+
+# Fit model
+earlystopper = EarlyStopping(patience=5, verbose=1)
+checkpointer = ModelCheckpoint(args.output, verbose=1)#, save_best_only=True)
+results = model.fit(tx,
+                    [ty, tc, to, tdx1, tdy1, tdx2, tdy2],
+                    validation_split=0.0,
+                    batch_size=16,
+                    epochs=args.epochs,
+                    callbacks=[earlystopper, checkpointer])
+
